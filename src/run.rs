dc09ea68ffb3552caf59858e626cd283a8258f20
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-11-21 17:42:29 +0000

    Implemented polygon distance

--- src/run.rs
+++ src/run.rs
@@ -1,7 +1,7 @@
 // Geospatial Modelling
 
 use crate::crs::{ update_point_crs, update_poly_crs };
-use crate::distance::{ DistanceMethod, find_closest_point, point_distance };
+use crate::distance::{ DistanceMethod, find_closest_point, point_distance, polygon_distance };
 
 use crs_definitions as crs_refs;
 use geo::{ polygon, Point, Polygon };
@@ -17,15 +17,21 @@ pub fn rs_geo() {
 
     // Create a poly
     let polygon:Polygon<f64> = polygon![
-        (x: 325113.5269645548, y: 673695.0227932289),
+        (x: 225113.5269645548, y: 673695.0227932289),
         (x: 325113.5269645948, y: 673695.0227932489),
         (x: 325113.5269646148, y: 673695.0227932689),
-        (x: 325113.5269645548, y: 673695.0227932289),
+    ];
+
+    let polygon_alt:Polygon<f64> = polygon![
+        (x: 335113.5269645548, y: 773695.0227932289),
+        (x: 335113.5269645948, y: 773695.0227932489),
+        (x: 335113.5269646148, y: 773695.0227932689),
     ];
 
     // Transform
     let point_tf = update_point_crs(point, &active_crs, &target_crs);
     let poly_tf = update_poly_crs(&polygon, &active_crs, &target_crs);
+    let poly_alt_tf = update_poly_crs(&polygon_alt, &active_crs, &target_crs);
     
 
     // Closest point
@@ -35,15 +41,18 @@ pub fn rs_geo() {
     let dist_h = point_distance(
         &point_tf, 
         &closest, 
-        DistanceMethod::Haversine,
+        &DistanceMethod::Haversine,
     );
 
     let dist_g = point_distance(
         &point_tf, 
         &closest, 
-        DistanceMethod::Geodesic,
+        &DistanceMethod::Geodesic,
     );
 
+    // Poly to poly dist 
+    let poly_dist_h = polygon_distance(&poly_tf, &poly_alt_tf, &DistanceMethod::Haversine);
+    let poly_dist_g = polygon_distance(&poly_tf, &poly_alt_tf, &DistanceMethod::Geodesic);
 
     println!("Transformed : {:?}", point_tf);
     println!("Transformed : {:?}", poly_tf);
@@ -52,9 +61,16 @@ pub fn rs_geo() {
     println!("Closest Points : \n- {:?} \n- {:?}", point_tf, closest);
     println!();
     
+    println!("Point to Point");
     println!("Haversine distance (km) : {:?}", (dist_h / 1000.0));
     println!("Geodesic distance (km) : {:?}", (dist_g / 1000.0));
     println!("Variance (%) : {:?}", 100.0 * ((dist_g - dist_h) / dist_g) );
     println!();
     
+    println!("Polygon to Polygon");
+    println!("Haversine distance (km) : {:?}", (poly_dist_h / 1000.0));
+    println!("Geodesic distance (km) : {:?}", (poly_dist_g / 1000.0));
+    println!("Variance (%) : {:?}", 100.0 * ((poly_dist_g - poly_dist_h) / poly_dist_g) );
+    println!();
+
 }
