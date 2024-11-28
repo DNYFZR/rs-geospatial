6688deb5b59777dedc01c05d78a0ddc5df3d557e
Danny <76977155+DNYFZR@users.noreply.github.com>
2024-11-28 12:31:39 +0000

    Implemented distance matrix model

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ mod utils;
 
 use std::time::Instant;
 
+use geo::{Point, Polygon};
+
 fn main() {
-    // run::rs_geo();
 
     println!("Requesting bathing water polygons from SEPA GIS system...");
     let start = Instant::now();
@@ -27,33 +28,41 @@ fn main() {
     println!("server response & data extraction completed in : {:?}", duration);
     println!("Dataset CRS : {:?}", bathing_waters::Points::CRS.code);
 
-    for i in 0..1 {
-        let point_set = &points[i];
-        let poly_set = &polys[i];
+    // Extract location & geometry for points & poly's
+    let point_vec:Vec<(String, Point)> = points.iter().map(|p| {
+        let point = spatial::update_point_crs(
+            p.shape, 
+            &crs_definitions::EPSG_27700,
+            &crs_definitions::EPSG_4326
+        );
+
+        return (String::from(&p.description), point);
+    }).collect();
+
+    let poly_vec:Vec<(String, Polygon)> = polys.iter().map(|p| {
+        let poly = spatial::update_poly_crs(
+            &p.shape.0[0], 
+            &crs_definitions::EPSG_27700, 
+            &crs_definitions::EPSG_4326
+        );
+
+        return (String::from(&p.description), poly);
+    }).collect();
+
+    // Calculate distance matrix
+    let mut dist_vec = vec![];
+    let start = Instant::now();
     
-        let object_id = &poly_set.object_id;
-        let poly = &poly_set.shape.0[0];
-        let point = &point_set.shape;
-        let bw_id = &poly_set.bw_id;
-        let description = &poly_set.description;
-        let year = &poly_set.year;
-        let current = &poly_set.current;
-        let class_id = &poly_set.class_id;
-        let class_description = &poly_set.class_description;
-        let bw_url = &poly_set.bw_url;
-    
-        println!("Entry {i} :
-            object_id = {object_id}
-            description = {description}
-            year = {year}
-            current = {current}
-            class_id = {class_id}
-            class_description = {class_description}
-            bw_url = {bw_url}
-            point_id = {bw_id}
-            point = {:?};
-            poly_id = {},
-            polygon = {:?}
-        ", point, poly_set.bw_id, poly);
+    for (point_loc, point) in &point_vec {
+        for (poly_loc, poly) in &poly_vec {
+            let dist = spatial::point_polygon_distance(point, poly, &spatial::DistanceMethod::Haversine);
+            dist_vec.push((point_loc, poly_loc, dist));
+        }
     }
+    let duration = start.elapsed();
+    println!("distance analysis complete in : {:?} for {} pairs", duration, dist_vec.len());
+    
+    // Sort by dist (low to high) 
+    dist_vec.sort_by(|a, b| a.2.partial_cmp(&b.2).unwrap());
+    println!("{:#?}", &dist_vec[0..100]);
 }
